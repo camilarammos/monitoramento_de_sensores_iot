6d9bc5c5fd5d08e1942bc5b78924ee3c686b0634
Ubuntu <ubuntu@ip-172-31-1-207.ec2.internal>
2025-08-04 21:18:34 +0000

    Ajustando consumer

--- spark/iot_consumer_spark_mysql.py
+++ spark/iot_consumer_spark_mysql.py
@@ -52,30 +52,12 @@ json_df = df.selectExpr("CAST(value AS STRING)") \
     .select(from_json(col("value"), schema).alias("data")) \
     .select("data.*")
 
-# =============================
-# Função de Escrita no MySQL
-# =============================
-def write_to_mysql(batch_df, epoch_id):
-    try:
-        logging.info(f"Processando batch {epoch_id} com {batch_df.count()} registros.")
-        batch_df.write.format("jdbc") \
-            .option("url", "jdbc:mysql://mysql:3306/sensores") \
-            .option("driver", "com.mysql.cj.jdbc.Driver") \
-            .option("dbtable", "leituras") \
-            .option("user", "root") \
-            .option("password", "root") \
-            .mode("append") \
-            .save()
-        logging.info(f"Batch {epoch_id} salvo no MySQL com sucesso.")
-    except Exception as e:
-        logging.error(f"Erro ao salvar batch {epoch_id}: {e}")
-
 # =====================
 # ▶️ Inicia o Streaming
 # =====================
 logging.info("Iniciando streaming de leitura...")
 
-if __name__=="__main__":
+if __name__ == "__main__":
     query = json_df.writeStream.foreachBatch(write_to_mysql).start()
     query.awaitTermination()
 
